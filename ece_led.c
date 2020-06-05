ec31516cef31b6dcdb95373ba8335941f1a3285e
Devon <dmickels@pdx.edu>
2020-06-05 11:26:31 -0700

    init commit

--- ece_led.c
+++ ece_led.c
@@ -1,9 +1,9 @@
 /*
  * Devon Mickels
- * 4/20/2020
+ * 6/5/2020
  * ECE 373
  *
- * Char Driver
+ * Interrupts and Descriptors
  */
 
 #include <linux/module.h>
@@ -14,26 +14,51 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/pci.h>
+#include <linux/workqueue.h>
+#include <linux/interrupt.h>
 
 #include <linux/kernel.h>
 
 #define DEVCNT 1
 #define DEVNAME "ece_led"
 
-#define LEDOFFSET 0xE00
- #define REG_MASK 0xFFFFFFF0
- #define LED_ON 0b1110
- #define LED_OFF 0b1111
+#define E1000_RECEIVE_BUF  0x00100
+#define E1000_RECEIVE_BASE_LOW 0x02800
+#define E1000_RECEIVE_BASE_HIGH 0x02804
+#define E1000_RECEIVE_LENGTH 0x02808
+#define E1000_RECEIVE_HEAD 0x2810
+#define E1000_RECEIVE_TAIL 0x2818
+#define E1000_DETECT_INTERRUPT 0x02C00
+
+#define E1000_LEDCTL	0x00E00	/* LED Control - RW */
+ 
+#define REG_MASK_LED0 0xFFFFFFF0
+#define REG_MASK_LED1 0xFFFFF0FF
+#define REG_MASK_LED2 0xFFF0FFFF
+#define REG_MASK_LED3 0xF0FFFFFF
+#define LED_ON 0b1110
+#define LED_OFF 0b1111
+ 
+#define GOOD_PACKETS_RECEIVED_REG 0x04074
+ 
+#define E1000_LEDCTL_LED0_SHIFT		0
+#define E1000_LEDCTL_LED1_SHIFT		8
+#define E1000_LEDCTL_LED2_SHIFT		16
+#define E1000_LEDCTL_LED3_SHIFT		24
 
 //Func declarations
 static int ece_led_open(struct inode *inode, struct file *file);
 static int ece_led_close(struct inode *inode, struct file *file);
 static int ece_led_probe(struct pci_dev *pdev, const struct pci_device_id *ent);
+static int ece_led_tx_setup(void);
 static int __init ece_led_init_module(void);
 static void __exit ece_led_exit_module(void);
 static void ece_led_remove(struct pci_dev *pdev);
 static ssize_t ece_led_read(struct file *file, char __user *buf, size_t len, loff_t *offset);
 static ssize_t ece_led_write(struct file *file, const char __user *buf, size_t len, loff_t *offset);
+static irqreturn_t ece_irq_handler(int irq, void *data);
+static void turnOnLEDs(void);
+static void turnOffLEDs(void);
 
 //End Func declarations
 
@@ -60,6 +85,28 @@ static struct myPCI_dev {
 	void *hw_addr;
 } myPCI_dev;
 
+static struct ece_led_desc{
+	_le64 buffer_addr;
+	union{
+		_le32 data;
+		struct{
+			_le16 length;
+			u8 cso;
+			u8 cmd;
+		} flags;
+	} lower;
+	union{
+		_le32 data;
+		struct{
+			u8 status;
+			u8 css;
+			_le16 special;
+		} fields;
+	} upper;
+};
+
+struct ece_led_desc[16] descriptors;
+
 /* File operations for our device */
 static struct file_operations mydev_fops = {
 	.owner = THIS_MODULE,
@@ -74,56 +121,11 @@ MODULE_DEVICE_TABLE(pci, ece_led_pci_tbl);
 static dev_t mydev_node;
 static struct class *c1;
 
-static int blink_rate = 2;
-module_param(blink_rate, int, S_IRUSR | S_IWUSR);
-
-int isOn = 0;
-
-//Timer handlers
-static struct myTimer_t {
-	unsigned long jiff;
-	struct timer_list blink_timer;
-} my_timer;
-
-static void blink_timer_cb(struct timer_list *t)
-{
-	u32 reg;
-	//u32 isOn;
-	struct myTimer_t *val = from_timer(val, t, blink_timer);
-
-	/* now mess with the data */
-	val->jiff = jiffies;
-	
-	//Handle blink of LED
-	reg = readl(myPCI_dev.hw_addr + LEDOFFSET);
-	
-	//isOn = reg & REG_MASK;
-	if(isOn == 1){
-		//Turn off LED
-		reg = (reg & REG_MASK) | LED_OFF;
-		isOn = 0;
-	}else{
-		//Turn on LED
-		reg = (reg & REG_MASK) | LED_ON;
-		isOn = 1;
-	}
-	
-	writel( reg, myPCI_dev.hw_addr  + LEDOFFSET);
-	
-	/* and restart the timer */
-	mod_timer(&my_timer.blink_timer, (jiffies + ((1/blink_rate) * HZ)));
-}
-
 //Device registration handlers
 
 static int ece_led_open(struct inode *inode, struct file *file)
 {
-	my_timer.jiff = jiffies;
 
-	timer_setup(&my_timer.blink_timer, blink_timer_cb, 0);
-
-	mod_timer(&my_timer.blink_timer, (jiffies + ((1/blink_rate) * HZ)));
-	
 	printk(KERN_INFO "Successfully opened ece_led!\n");
 
 	return 0;
@@ -131,9 +133,6 @@ static int ece_led_open(struct inode *inode, struct file *file)
 
 static int ece_led_close(struct inode *inode, struct file *file)
 {
-	//Exit timer
-	del_timer_sync(&my_timer.blink_timer);
-	
 	printk(KERN_INFO "Successfully closed ece_led!\n");
 
 	return 0;
@@ -156,9 +155,61 @@ static int ece_led_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	
 	myPCI_dev.hw_addr  = pci_ioremap_bar(pdev, 0);
 	
+	ece_led_tx_setup();
+	
 	printk(KERN_INFO "ece_led successfully registered\n");
 	
 	return 0;
+}
+
+static int ece_led_tx_setup(void){
+	
+	int err;
+	//request an IRQ
+	err = request_irq(myPCI_dev->pdev->irq, ece_irq_handler, 0, "ece_int", data); //Data is the data passed into IRQ handler
+	
+	return err;
+}
+
+static irqreturn_t ece_irq_handler(int irq, void *data){
+	u32 cause;
+	
+	cause = readl(myPCI_dev->hw_addr + REG_IRQ_CAUSE);
+	
+	turnOnLEDs();
+	
+	switch(cause){
+		case 0x01: printk( KERN_INFO "Test\n");
+			break;
+		default:
+			break;
+	}
+	
+	return IRQ_HANDLED;
+}
+
+static void turnOnLEDs(void){
+	u32 ledctl;
+	
+	//Read value of LEDS
+	ledctl = readl(myPCI_dev.hw_addr + E1000_LEDCTL);
+	
+	ledctl = (ledctl & REG_MASK_LED0) | LED_ON << E1000_LEDCTL_LED0_SHIFT);
+	ledctl = (ledctl & REG_MASK_LED2) | LED_ON << E1000_LEDCTL_LED2_SHIFT);
+	
+	writel( ledctl, myPCI_dev.hw_addr  + E1000_LEDCTL);
+}
+
+static void turnOffLEDs(void){
+	u32 ledctl;
+	
+	//Read value of LEDS
+	ledctl = readl(myPCI_dev.hw_addr + E1000_LEDCTL);
+	
+	ledctl = (ledctl & REG_MASK_LED0) | LED_OFF << E1000_LEDCTL_LED0_SHIFT);
+	ledctl = (ledctl & REG_MASK_LED2) | LED_OFF << E1000_LEDCTL_LED2_SHIFT);
+	
+	writel( ledctl, myPCI_dev.hw_addr  + E1000_LEDCTL);
 }
 
 static int __init ece_led_init_module(void)
@@ -202,6 +253,8 @@ module_init(ece_led_init_module);
 
 static void __exit ece_led_exit_module(void)
 {	
+	free_irq(myPCI_dev->pdev->irq, data);
+
 	//Exit device
 	cdev_del(&mydev.my_cdev);
 	
